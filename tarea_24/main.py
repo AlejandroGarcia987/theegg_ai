57b0e17eedd3c323f61883679ac99482ff658e2f
Alex Garcia <agarcia@amcsa.es>
2021-06-01 21:13:40 +0200

    Código acabado. Actualizar diccionario pendiente

--- tarea_24/main.py
+++ tarea_24/main.py
@@ -1,1 +1,16 @@
 ##Tarea 24. Programar un conversor "Analógico digital"
+numero = input("introduce un número decimal: ")
+numero = numero.replace(",",".")
+
+#comprobamos que no se haya introducido un String y lo pasamos a número tipo int para poder convertirlo a binario,
+#de lo contrario, mandamos un mensaje
+try:
+    numero = int(numero)
+except:
+    print("No has introducido un número válido");
+    exit()
+
+numero_binario = bin(numero)
+
+print("El numero binario es: ")
+print(numero_binario)
